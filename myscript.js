9d47bb7a4a1d0e5a0a530f01a6e1570a366d4652
shayanali7582 <shayanali7582@gmail.com>
2023-09-01 20:17:51 +0500

    javascript hw 1

---
+++ myscript.js
@@ -0,0 +1,61 @@
+                        //? Chapter 1
+
+//? Question No.1
+// alert("Welcome to EpicXpedition!");
+
+//? Question No.2
+//alert("Error! Please enter a valid Password.");
+
+//? Question No.3
+// alert("Welcome to JS land...\nHappy Coding!");
+
+//? Question No.4
+// alert("Welcome to JS land...");
+//alert("Happy Coding!");
+
+//? Question No.5
+//image is attached
+
+//? Question No.6
+//already performed
+
+//? Question No.7
+//images are attached
+
+                                //? Chapter 2
+
+//? Question No.1
+//var username;
+
+//? Question No.2
+// var myName = "Shayan Ali Rajani";
+
+//? Question No.3
+// var message = "Hello World";
+// alert(message);
+
+//? Question No.4
+// var StdName = "Jhone Doe";
+// alert(Student's Name);
+// var StdAge = "15 year old";
+// alert(Student's Age);
+// var Subject = "Certified Mobile Application Development";
+// alert(Subject);
+
+//? Question No.5
+// var PyramicPattern = "PIZZA\nPIZZ\nPIZ\nPI\nP";
+// alert(PyramicPattern);
+
+//? Question No.6
+// var email = "shayanai@mukhti.com";
+// alert("My email address is " + email);
+
+//? Question No.7
+// var book = "A smarter way to learn JavaScript.";
+// alert("I am trying to learn from the Book. " + book);
+
+//? Question No.8
+// image is attached
+
+//? Question No.9
+// images are attached
